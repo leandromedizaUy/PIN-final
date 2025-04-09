5288794ee34a95855bcc0d45b54b42fe4dea12f0
Leandro <leandromediza@gmail.com>
2025-04-09 13:29:35 -0300

    fix

--- ec2/main.tf
+++ ec2/main.tf
@@ -42,6 +42,6 @@ resource "aws_iam_role_policy_attachment" "ec2_policy" {
 }
 
 resource "aws_iam_instance_profile" "ec2_admin" {
-  name = "ec2-admin-profile2"
+  name = "ec2-admin-iam-profile"
   role = aws_iam_role.ec2_role.name
 }
