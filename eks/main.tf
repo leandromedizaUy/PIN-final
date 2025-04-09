48acccf69872d0eba890fabc1b5bd30011559606
Leandro <leandromediza@gmail.com>
2025-04-09 09:17:58 -0300

    fix eks vpc subnet issue2

--- eks/main.tf
+++ eks/main.tf
@@ -2,7 +2,7 @@ module "eks" {
   source          = "terraform-aws-modules/eks/aws"
   cluster_name    = "eks-mundos-e"
   cluster_version = "1.27"
-  vpc_subnet_ids = module.vpc.public_subnets
+  subnet_ids = module.vpc.public_subnets
   vpc_id          = module.vpc.vpc_id
 
   eks_managed_node_groups = {
