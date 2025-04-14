ea6b2245914811c69f97a1ce2fda15e7494dc488
Leandro <leandromediza@gmail.com>
2025-04-14 12:10:44 -0300

    improve deploy workflows

---
+++ scripts/deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e
+
+echo "Configurando acceso al cluster EKS..."
+aws eks update-kubeconfig \
+  --region "$AWS_REGION" \
+  --name "$EKS_CLUSTER_NAME"
+
+echo "Verificando herramientas..."
+kubectl version --client
+helm version
+
+echo "Aplicando manifiesto de NGINX..."
+kubectl apply -f ~/k8s/manifiesto_nginx.yml
+
+echo "Instalando Ingress Controller con Helm..."
+helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+helm repo update
+helm install ingress-nginx ingress-nginx/ingress-nginx \
+  --namespace ingress-nginx \
+  --create-namespace
+
+echo "Aplicando reglas de ingreso para NGINX..."
+kubectl apply -f ~/k8s/ingress_rules.yml
+
+echo "Instalando Prometheus..."
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo update
