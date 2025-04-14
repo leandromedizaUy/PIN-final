5123c620cafe1c0cf88bee5b8255438ca36e4164
Leandro <leandromediza@gmail.com>
2025-04-14 18:14:53 -0300

    prometheus y grafana

---
+++ scripts/deploy_monitoring.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+set -e
+
+echo "Configurando acceso al cluster EKS..."
+aws eks update-kubeconfig \
+  --region "$AWS_REGION" \
+  --name "$EKS_CLUSTER_NAME"
+
+echo "Instalando Prometheus..."
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo update
+helm install prometheus prometheus-community/prometheus \
+  --namespace monitoring \
+  --create-namespace
+
+echo "Instalando Grafana..."
+helm repo add grafana https://grafana.github.io/helm-charts
+helm repo update
+helm install grafana grafana/grafana \
+  --namespace monitoring \
+  --create-namespace \
+  --set service.type=LoadBalancer \
+  --set adminPassword='admin123'
+
+echo "Monitoring stack desplegado con éxito"
+
+echo "Esperando IP pública del LoadBalancer de Grafana..."
+LB_IP=""
+for i in {1..10}; do
+  LB_IP=$(kubectl get svc -n monitoring grafana -o jsonpath="{.status.loadBalancer.ingress[0].hostname}")
+  if [[ -n "$LB_IP" ]]; then
+    break
+  fi
+  echo "⌛ Aún sin IP... esperando..."
+  sleep 10
+done
+
+echo "Grafana disponible en: http://$LB_IP"
